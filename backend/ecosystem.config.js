52722c73560ab435e1987c22ec416f044a0545c7
tncn1122 <tncnhan@gmail.com>
2021-12-04 18:28:13 +0700

    change exec mode

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,19 +1,7 @@
 module.exports = {
   apps : [{
     script: 'app.js',
-    watch: '.'
+    watch: '.',
+    exec_mode : "cluster"
   },],
-
-  deploy: {
-    production: {
-      user: 'tncn1122',
-      host: '54.254.154.61',
-      key: 'deploy.key',
-      ref: 'origin/main',
-      repo: 'git@github.com:tncn1122/UIS.git',
-      path: '/var/www/html/UIS',
-      'post-deploy':
-        'yarn install && pm2 reload ecosystem.config.js --env production && pm2 save',
-    },
-  },
 };
