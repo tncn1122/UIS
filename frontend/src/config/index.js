956ac76c728f97f3e9632497073660535829707d
tncn1122 <tncnhan@gmail.com>
2021-12-16 04:20:35 +0700

    Update build file

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,2 +1,2 @@
-const host = process.env.REACT_APP_ENV === 'development' ? 'http://54.254.154.61' : 'http://localhost:3000'
+const host = process.env.REACT_APP_ENV === 'development' ? 'http://54.254.154.61:3000' : 'http://localhost:3000'
 export const API_URL = `${host}/api`
